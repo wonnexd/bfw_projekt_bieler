86ced35ee6698039fe373acb6682b1f798634a6a
Timow <85105048+wonnexd@users.noreply.github.com>
2022-06-16 10:05:58 +0200

    first commit

---
+++ bfw_projekt_bieler/Verarbeitung.php
@@ -0,0 +1,51 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "bieler";
+
+    $input1 = $_POST['input1'];
+    $input2 = $_POST['input2'];
+    $input3 = $_POST['input3'];
+    $input4 = $_POST['input4'];
+
+    try {
+        $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+        // set the PDO error mode to exception
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        $sql = "UPDATE ausgabedb SET text1='$input1', text2='$input2', text3='$input3', text4='$input4' WHERE id=17";
+
+        // Prepare statement
+        $stmt = $conn->prepare($sql);
+
+        // execute the query
+        $stmt->execute();
+
+        // echo a message to say the UPDATE succeeded
+        echo $stmt->rowCount() . " records UPDATED successfully";
+    } catch (PDOException $e) {
+        echo $sql . "<br>" . $e->getMessage();
+    }
+
+    $conn = null;
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    fertig
+    <a href="Ausgabe.php">Zur Ausgabe</a>
+</body>
+
+</html>
